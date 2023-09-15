1d430335372b22bcc660bed65f1a2995fa35c364
Charlie <charlienino@gmail.com>
2023-09-15 16:20:59 -0700

    Add styling and other pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root 'pages#welcome'
+
+  get 'portfolio', to: 'pages#portfolio'
+
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
 end
 
